118c53b1ffa3ed6ed652c82e4ed9572bafd3640e
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-08-11 13:34:07 +0100

    'collectstatic-fixed-upload-bug'

---
+++ assets/js/upload.js
@@ -0,0 +1,63 @@
+const uploadForm = document.querySelector('.upload_label');
+const uploadInput = document.querySelector('#imageForm');
+const previewBox = document.querySelector('.preview_img');
+const copyText = document.querySelector('#copy_text');
+const copyBtn = document.querySelector('#copy_btn');
+const isUploadingText = document.querySelector('.isUploading');
+
+// DRAG AND DROP FUNCTIONALITY
+function previewFile(file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function (e) {
+        const img = document.createElement('img');
+        img.src = reader.result;
+        img.alt = file.name;
+        previewBox.appendChild(img);
+    }
+}
+function uploadFile(file) {
+    const token = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+    const formData = new FormData();
+    formData.append('image', file);
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            window.location.href = '/';
+            window.location.reload();
+        }
+    }
+    xhr.open('POST', '/my-favicons/');
+    xhr.setRequestHeader('X-CSRFToken', token);
+    xhr.send(formData);
+    isUploadingText.innerHTML = 'Uploading...';
+}
+function highlightZone(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    uploadForm.classList.add('over');
+}
+function unhighlightZone(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    uploadForm.classList.remove('over');
+}
+['dragenter', 'dragover'].forEach(eventName => { uploadForm.addEventListener(eventName, highlightZone) });
+['dragleave', 'drop'].forEach(eventName => { uploadForm.addEventListener(eventName, unhighlightZone) });
+uploadForm.addEventListener('drop', handleDrop);
+function handleDrop(e) {
+    const files = e.dataTransfer.files;
+    const file = files[0];
+    previewFile(file);
+    uploadFile(file);
+}
+uploadInput.addEventListener('change', () => {
+    const file = imageForm.files[0];
+    previewFile(file);
+})
+
+// COPY TEXT FUNCTIONALITY
+text = copyText.innerText;
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(text);
+});
--- core/views.py
+++ core/views.py
@@ -22,20 +22,24 @@ def imageUploadView(request):
     if request.method == 'POST':
         list(messages.get_messages(request))
         if request.FILES or request.POST:
-            image = request.FILES['image'] or request.POST['image']
-            imgid = f'{request.user}_image-{secrets.token_urlsafe(10)}'
-            zipid = f'{request.user}_favicon-{secrets.token_urlsafe(10)}'
-            url_list = createFavicons(image, imgid, zipid)
-            res_img, res_zip = url_list.values()
-            image = Image.objects.create(
-                user_id=request.user, image_name=imgid, image_url=res_img
-            )
-            image.save()
-            favicon = Favicon_Zip.objects.create(
-                user_id=request.user, image_id=image, favicon_name=zipid, favicon_zip_url=res_zip
-            )
-            favicon.save()
-            return redirect('core:upload')
+            try:
+                image = request.FILES['image'] or request.POST['image']
+                imgid = f'{request.user}_image-{secrets.token_urlsafe(10)}'
+                zipid = f'{request.user}_favicon-{secrets.token_urlsafe(10)}'
+                url_list = createFavicons(image, imgid, zipid)
+                res_img, res_zip = url_list.values()
+                image = Image.objects.create(
+                    user_id=request.user, image_name=imgid, image_url=res_img
+                )
+                image.save()
+                favicon = Favicon_Zip.objects.create(
+                    user_id=request.user, image_id=image, favicon_name=zipid, favicon_zip_url=res_zip
+                )
+                favicon.save()
+                return redirect('core:upload')
+            except:
+                messages.error(request, 'Image not provided')
+                return render(request, 'core/upload.html')
         else:
             messages.error(request, 'Image not provided')
             return render(request, 'core/upload.html')
