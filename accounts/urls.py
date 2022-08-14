546e9b4d71612b5da3f7eec8d5c4a5478e24403b
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-08-14 15:42:44 +0100

    'pass-reset-2'

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,20 +9,18 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 
-
     path('reset_password/',
-         auth_views.PasswordResetView.as_view(template_name='accounts/pass1.html', success_url=reverse_lazy('accounts:password_reset_done')),
+         auth_views.PasswordResetView.as_view(template_name='accounts/pass1.html',
+                                              success_url=reverse_lazy('accounts:password_reset_done')),
          name='reset_password'),
-
-    path('reset_password_sent/',
+    path('password_reset_done/',
          auth_views.PasswordResetDoneView.as_view(template_name='accounts/pass2.html'),
          name='password_reset_done'),
-
-    path('password_change/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/pass3.html', success_url=reverse_lazy('accounts:password_reset_complete')),
+    path('password_reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='accounts/pass3.html',
+                                                    success_url=reverse_lazy('accounts:password_reset_complete')),
          name='password_reset_confirm'),
-
-    path('reset_password_complete/',
-         auth_views.PasswordResetCompleteView.as_view(),
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/pass4.html'),
          name='password_reset_complete'),
-]
+        ]
